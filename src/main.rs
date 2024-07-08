1e554ee4d9ffbdcdebffde627efd96f52b95789a
HaNaK0 <hampus@hampushuledal.com>
2024-07-08 22:58:55 +0200

    First temp version
    add header with my name
    add text explaining what they are looking at

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,39 @@
 use bevy::{
+    color::palettes::css::{MAROON, RED, YELLOW},
     prelude::*,
-    window::{WindowMode, WindowResized},
+    text::TextLayoutInfo,
+    ui::widget::TextFlags,
+    window::WindowMode,
     winit::WinitSettings,
 };
-use xml_texture_atlas::{XMLTextureAtlas, XMLTextureAtlasComponent, XMLTextureAtlasPlugin};
 
 mod xml_texture_atlas;
 
+#[derive(Debug, Component)]
+struct RootNode;
+
 fn main() {
     App::new()
-        .add_plugins((
-            DefaultPlugins
-                .set(WindowPlugin {
-                    primary_window: Some(Window {
-                        mode: WindowMode::Fullscreen,
-                        //resolution: WindowResolution::new(1920.0, 1080.0),
-                        title: "Hampus Huledals Portfolio".to_string(),
-                        resizable: true,
-                        decorations: true,
-                        transparent: false,
-                        focused: true,
-                        fit_canvas_to_parent:true,
-                        ..Default::default()
-                    }),
+        .add_plugins((DefaultPlugins
+            .set(WindowPlugin {
+                primary_window: Some(Window {
+                    mode: WindowMode::Windowed,
+                    //resolution: WindowResolution::new(1920.0, 1080.0),
+                    title: "Hampus Huledals Portfolio".to_string(),
+                    resizable: true,
+                    decorations: true,
+                    transparent: false,
+                    focused: true,
+                    fit_canvas_to_parent: true,
                     ..Default::default()
-                })
-                .set(AssetPlugin {
-                    mode: AssetMode::Unprocessed,
-                    ..Default::default()
-                })
-                .set(ImagePlugin::default_nearest()),
-            XMLTextureAtlasPlugin,
-        ))
+                }),
+                ..Default::default()
+            })
+            .set(AssetPlugin {
+                mode: AssetMode::Unprocessed,
+                ..Default::default()
+            })
+            .set(ImagePlugin::default_nearest()),))
         .insert_resource(WinitSettings::desktop_app())
         .add_systems(Startup, setup)
         .run();
@@ -40,47 +42,128 @@ fn main() {
 fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
     commands.spawn(Camera2dBundle::default());
 
-    let text_style = TextStyle {
-        font_size: 20.,
-        ..default()
-    };
-
-    let xml_atlas: Handle<XMLTextureAtlas> = asset_server.load("spritesheets/blueSheet.xml");
-    let texture_handle: Handle<Image> = asset_server.load("spritesheets/blueSheet.xml#image");
-    let texture_atlas_handle: Handle<TextureAtlasLayout> =
-        asset_server.load("spritesheets/blueSheet.xml#layout");
+    let header_font = asset_server.load::<Font>("./fonts/Ubuntu/Ubuntu-Bold.ttf");
+    let body_font = asset_server.load::<Font>("./fonts/Ubuntu/Ubuntu-Regular.ttf");
 
     //root node
     commands
-        .spawn(NodeBundle {
-            style: Style {
-                width: Val::Percent(100.0),
-                height: Val::Percent(100.0),
-                flex_direction: FlexDirection::Column,
-                justify_content: JustifyContent::Center,
-                align_items: AlignItems::Center,
-                row_gap: Val::Px(text_style.font_size * 2.),
+        .spawn((
+            NodeBundle {
+                style: Style {
+                    margin: UiRect::all(Val::Px(0.)),
+                    align_self: AlignSelf::Stretch,
+                    flex_wrap: FlexWrap::Wrap,
+                    justify_content: JustifyContent::FlexStart,
+                    align_items: AlignItems::FlexStart,
+                    align_content: AlignContent::FlexStart,
+                    ..default()
+                },
                 ..default()
             },
-            ..default()
-        })
-        .with_children(|parent| {
-            parent.spawn((
-                ImageBundle {
-                    style: Style {
-                        width: Val::Px(190.),
-                        height: Val::Px(49.),
+            Name::new("root"),
+            RootNode,
+        ))
+        .with_children(|builder| {
+            // Add header
+            builder
+                .spawn((
+                    NodeBundle {
+                        style: Style {
+                            margin: UiRect::bottom(Val::Px(16.)),
+                            align_self: AlignSelf::Stretch,
+                            justify_content: JustifyContent::FlexStart,
+                            align_items: AlignItems::Center,
+                            align_content: AlignContent::FlexStart,
+                            width: Val::Vw(100.),
+                            height: Val::Vh(10.),
+                            ..default()
+                        },
+                        background_color: Color::oklch(0.31, 0.01, 271.22).into(),
                         ..default()
                     },
-                    image: UiImage::new(texture_handle),
-                    ..Default::default()
-                },
-                TextureAtlas::from(texture_atlas_handle) ,
-                XMLTextureAtlasComponent {
-                    name: "blue_button00.png".to_string(),
-                    atlas: xml_atlas,
-                },
-            ));
+                    Name::new("Header"),
+                ))
+                .with_children(|builder| {
+                    builder.spawn((
+                        TextBundle {
+                            text: Text::from_section(
+                                "Hampus Huledal",
+                                TextStyle {
+                                    font: header_font.clone(),
+                                    font_size: 32.0,
+                                    color: Color::WHITE,
+                                },
+                            ),
+                            style: Style {
+                                margin: UiRect::left(Val::Px(16.0)),
+                                ..default()
+                            },
+                            ..default()
+                        },
+                        Name::new("Header Text"),
+                    ));
+                });
+
+            // Add Body
+            builder
+                .spawn((
+                    Name::new("Body"),
+                    NodeBundle {
+                        style: Style {
+                            align_self: AlignSelf::Stretch,
+                            justify_content: JustifyContent::Center,
+                            align_items: AlignItems::Center,
+                            align_content: AlignContent::FlexStart,
+                            flex_direction: FlexDirection::Column,
+                            height: Val::Percent(100.0),
+                            width: Val::Vw(100.0),
+                            ..default()
+                        },
+                        ..default()
+                    },
+                ))
+                .with_children(|builder| {
+                    let header_text = Text::from_sections([
+                        TextSection {
+                            value : "Welcome to my portfolio!".to_string(),
+                            style : TextStyle {
+                                font : header_font.clone(),
+                                font_size : 32.0,
+                                color: Color::srgba(1.0, 1.0, 1.0, 0.6)
+                            }
+                        },
+                    ]).with_justify(JustifyText::Center);
+
+                    builder.spawn(TextBundle {
+                        text: header_text,
+                        style: Style {
+                            margin: UiRect::all(Val::Px(5.0)),
+                            ..default()
+                        },
+                        ..default()
+                    });
+
+                    let body_text = Text::from_sections([
+                        TextSection {
+                            value : "It is made using bevy which is a game engine. You may wonder why i decided to use it? The answer is that i'm a game developer not a web developer and i wanted a project that both was useful and fun to do. Be aware that it is still very much under development".to_string(),
+                            style : TextStyle {
+                                font : body_font.clone(),
+                                font_size : 16.0,
+                                color: Color::srgba(1.0, 1.0, 1.0, 0.6)
+                            }
+                        },
+                    ]).with_justify(JustifyText::Center);
+
+                    builder.spawn(TextBundle {
+                        text: body_text,
+                        style: Style {
+                            max_width: Val::Percent(50.0),
+                            ..default()
+                        },
+                        ..default()
+                    });
+                });
+
+                
         });
 }
-
